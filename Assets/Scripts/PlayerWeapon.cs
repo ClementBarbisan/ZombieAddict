ed27315d9b888dfbf19d4eeaf6d5a810074b16a1
OwenDuffourg <48203107+OwenDuffourg@users.noreply.github.com>
2026-04-04 17:32:30 +0200

    Add Spider Enemy, Shoot Controller

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
@@ -7,12 +6,22 @@ public class PlayerWeapon : MonoBehaviour
     public float coolDownFire = .2f;
     [SerializeField] private Transform weaponPos;
     [SerializeField] private ParticleSystem hitVFX;
+    [SerializeField] private LineRenderer hitLine;
     
     private float _cooldownTimer;
     private bool _isFiring;
+    private Material _hitLineMat;
+    private Color _hitLineColor;
+    private float _alphaLineHit;
+
+    private void Start()
+    {
+        _hitLineMat = hitLine.material;
+    }
 
     public void HandleFire(bool isPressed)
     {
+        Debug.Log("isPress"+ isPressed);
         _isFiring = isPressed;
     }
 
@@ -22,8 +31,18 @@ public class PlayerWeapon : MonoBehaviour
 
         if (_isFiring && _cooldownTimer <= 0f)
         {
+            Debug.Log("FIRE");
             Shoot();
             _cooldownTimer = coolDownFire;
+        }
+        
+        if(_alphaLineHit > 0f)
+        {
+            _alphaLineHit -= Time.deltaTime * 5f;
+            if (_alphaLineHit < 0f)
+                _alphaLineHit = 0f;
+            
+            _hitLineMat.color = new Color(_hitLineColor.r, _hitLineColor.g, _hitLineColor.b, _alphaLineHit);
         }
     }
 
@@ -32,21 +51,13 @@ public class PlayerWeapon : MonoBehaviour
         if (Physics.Raycast(weaponPos.position, weaponPos.forward, out RaycastHit hit))
         {
             // VFX impact
-            if (hitVFX != null)
-            {
-                hitVFX.transform.position = hit.point;
-                hitVFX.transform.forward = hit.normal;
-                hitVFX.Emit(1);
-            }
-
-            Debug.Log("Fire 🔫");
-
-            Debug.DrawRay(
-                weaponPos.position,
-                weaponPos.forward * hit.distance,
-                Color.red,
-                0.5f
-            );
+            hitVFX.transform.position = hit.point;
+            hitVFX.transform.forward = hit.normal;
+            hitVFX.Emit(1);
+            
+            hitLine.SetPosition(0,weaponPos.position);
+            hitLine.SetPosition(1,hit.point);
+            _alphaLineHit = 1f;
         }
     }
 }
