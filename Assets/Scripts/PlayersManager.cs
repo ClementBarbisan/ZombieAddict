2eb9fdba87919b7dcdba79b1c385e165a167de88
OwenDuffourg <48203107+OwenDuffourg@users.noreply.github.com>
2026-04-04 13:18:56 +0200

    PlayersManager add string name

--- Assets/Scripts/PlayersManager.cs
+++ Assets/Scripts/PlayersManager.cs
@@ -4,7 +4,7 @@ public class PlayersManager : MonoBehaviour
 {
     [SerializeField] private PlayerController playerPrefab;
     
-    public PlayerController CreateNewPlayer()
+    public PlayerController CreateNewPlayer(string name)
     {
         return Instantiate(playerPrefab, new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f)),
             Quaternion.identity);
