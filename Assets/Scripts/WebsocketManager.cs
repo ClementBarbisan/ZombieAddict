67a680e3e81bb9af3b0b789e29a43ccface4fbe5
Clément Barbisan <c.barbisan@free.fr>
2026-04-05 17:26:15 +0200

    addd log players

--- Assets/Scripts/WebsocketManager.cs
+++ Assets/Scripts/WebsocketManager.cs
@@ -396,6 +396,7 @@ public class WebsocketManager : MonoBehaviour
             byte[] tmpBytes = Convert.FromBase64String(base64);
             Texture2D imgTexture = new Texture2D(256, 256);
             imgTexture.LoadImage(tmpBytes);
+            Debug.LogWarning("Name player = " + player.Value.nickname);
             _playersManager.SetupAvatar(imgTexture, player.Value.nickname, player.Value.clientId);
             if (player.Value.role == "survivor" && !_players.ContainsKey(player.Value.clientId))
             {
