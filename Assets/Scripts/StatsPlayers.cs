26692e0080bea750060cdc5ce9ded22f089791fd
Clément Barbisan <clement@xraddict.studio>
2026-04-06 16:10:35 +0200

    ui fix

--- Assets/Scripts/StatsPlayers.cs
+++ Assets/Scripts/StatsPlayers.cs
@@ -10,11 +10,10 @@ public class StatsPlayers : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void SetStats()
     {
-        nameText.text = stats.name + Environment.NewLine + "Accuracy : " + stats.accuracy + Environment.NewLine + "Damages taken : " + stats.damages +
-                        Environment.NewLine
+        nameText.text = stats.name + Environment.NewLine + Environment.NewLine + "Accuracy : " + stats.accuracy +
+                        Environment.NewLine + "Damages taken : " + stats.damages + Environment.NewLine
                         + "Damages to enemy : " + stats.damagesEnemy + Environment.NewLine + "Enemy killed : " +
-                        stats.enemyKilled +
-                        Environment.NewLine + "Shoot fired : " + stats.shootFired + Environment.NewLine + "Walk distance : " +
-                        stats.walkDistance;
+                        stats.enemyKilled + Environment.NewLine + "Shoot fired : " + stats.shootFired +
+                        Environment.NewLine + "Walk distance : " + stats.walkDistance;
     }
 }
